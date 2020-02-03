d513b5f774cb3205d2338fce31f7e7083f600d17
Tungtwister <austin.tung56@gmail.com>
2020-02-02 21:04:35 -0800

    Finished Base Product
    Finished Base Design, would like add a few more features to enhance experience

---
+++ Assets/Scripts/LaserScript.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserScript : MonoBehaviour
+{
+    public float mFireRate = 0.5f;
+    public float mFireRange = 50f;
+    public float mHitForce = 100f;
+    public int mLaserDamage = 100;
+
+    private LineRenderer mLaserLine;
+
+    private bool mLaserLineEnabled;
+
+    private WaitForSeconds mLaserDuration = new WaitForSeconds(0.05f);
+
+    private float mNextFire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mLaserLine = GetComponent<LineRenderer>();
+    }
+
+    private void Fire()
+    {
+        Transform cam = Camera.main.transform;
+
+        mNextFire = Time.time + mFireRate;
+
+        Vector3 rayOrigin = cam.position;
+
+        mLaserLine.SetPosition(0, transform.up * -10f);
+
+        RaycastHit hit;
+
+        if(Physics.Raycast( rayOrigin, cam.forward, out hit, mFireRange ))
+        {
+            mLaserLine.SetPosition(1, hit.point);
+        }
+        else
+        {
+            mLaserLine.SetPosition(1, cam.forward * mFireRange);
+        }
+
+        StartCoroutine(LaserFx());
+
+        if(Physics.Raycast(rayOrigin, cam.forward, out hit, mFireRange))
+        {
+            mLaserLine.SetPosition(1, hit.point);
+
+            CubeBehaviorScript cubeCtr = hit.collider.GetComponent<CubeBehaviorScript>();
+            if( cubeCtr != null)
+            {
+                if(hit.rigidbody != null)
+                {
+                    hit.rigidbody.AddForce(-hit.normal * mHitForce);
+
+                    cubeCtr.Hit(mLaserDamage);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Fire1") && Time.time > mNextFire)
+        {
+            Fire();
+        }
+    }
+
+    private IEnumerator LaserFx()
+    {
+        mLaserLine.enabled = true;
+
+        yield return mLaserDuration;
+        mLaserLine.enabled = false;
+    }
+
+
+
+
+}
+
---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Vuforia;
+
+public class SpawnScript : MonoBehaviour
+{
+    public GameObject mCubeObj;
+
+    public int mTotalCubes = 10;
+
+    public float mTimeToSpawn = 1f;
+
+    private GameObject[] mCubes;
+
+    private bool mPositionSet;
+
+    //Define the position of the object
+    //according to ARCamera position
+    private bool SetPosition()
+    {
+        Transform cam = Camera.main.transform;
+
+        transform.position = cam.forward * 10;
+        return true;
+    }
+
+
+    private IEnumerator ChangePosition()
+    {
+
+        yield return new WaitForSeconds(0.2f);
+        // Define the Spawn position only once
+        if (!mPositionSet)
+        {
+            // change the position only if Vuforia is active
+            if (VuforiaBehaviour.Instance.enabled)
+                SetPosition();
+        }
+    }
+
+    private IEnumerator SpawnLoop()
+    {
+        StartCoroutine(ChangePosition());
+
+        yield return new WaitForSeconds(0.2f);
+
+        int i = 0;
+        while ( i <= (mTotalCubes-1))
+        {
+            mCubes[i] = SpawnElement();
+            i++;
+            yield return new WaitForSeconds(Random.Range(mTimeToSpawn, mTimeToSpawn * 3));
+        }
+
+    }
+
+    private GameObject SpawnElement()
+    {
+        GameObject cube = Instantiate(mCubeObj, (Random.insideUnitSphere * 4) + transform.position, transform.rotation) as GameObject;
+
+        float scale = Random.Range(0.5f, 2f);
+
+        cube.transform.localScale = new Vector3(scale, scale, scale);
+        return cube;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine( SpawnLoop());
+
+        mCubes = new GameObject[mTotalCubes];
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
